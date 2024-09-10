20856ce89386b00e0ecdbdce4bce8b027fb86f4d
giovanni cortes <you@example.comgiocorcol@gmail.com>
2024-09-09 21:04:11 -0500

    reservation

---
+++ controllers/reservationController.js
@@ -0,0 +1,83 @@
+import User from "../models/Reservation.js";
+
+async function getAll(req, res) {
+  try {
+    const reservation = await Reservation.find({ deletedAt: null });
+    return res.json(users);
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json("No se encuentra reservas");
+  }
+}
+
+async function getById(req, res) {
+  try {
+    const reservation = await Reservation.findById(req.params.id);
+    return res.json(reservation);
+  } catch (reservation) {
+    console.log(error);
+    return res.status(404).json(" Reserva no encontrada");
+  }
+}
+
+
+
+async function create(req, res) {
+  try {
+    const newReservation = await reservation.create({
+        dateReservation: req.body.dateReservation,
+        timeReservation: req.body.timeReservation,
+        status: req.body.status,
+        priceTotal: req.body.priceTotal,
+    });
+
+    return res.status(201).json(newReservation);
+  } catch (error) {
+    console.log(error.errors.status.properties.message);
+    return res.status(501).json("Error en el servidor");
+  }
+}
+
+async function update(req, res) {
+  const reservationToUpdate = await UserReservation.findById(req.params.id);
+
+  if (reservationToUpdate !== null) {
+    const { dateReservation, timeReservation, status, priceTotal} = req.body;
+
+    reservationToUpdate.dateReservation = dateReservation || reservationUpdate.dateReservation;
+    reservationToUpdate.timeReservation = timeReservation || reservationToUpdate.timeReservation;
+    reservationToUpdate.status = status || reservationToUpdate.status;
+    reservationToUpdate.priceTotal = priceTotal|| reservationToUpdate.priceTotal;
+
+    await reservationToUpdate.save();
+
+    return res.json("Reserva actualizada");
+  } else {
+    return res.json("Reserva no exite");
+  }
+}
+
+async function destroy(req, res) {
+  try {
+    const reservationToDelete = await Reservation.findById(req.params.id);
+
+    if (reservationToDelete) {
+      reservationToDelete.deletedAt = Date.now();
+      reservationToDelete.save();
+
+      return res.json("La reserva se cancelo con exito");
+    } 
+    
+  } catch (err) {
+    console.log(err);
+    return res.status(404).json("No se encuentra reserva para cancelar");
+  }
+}
+
+export default {
+  getAll: getAll,
+  getById: getById,
+  create: create,
+  update: update,
+  destroy: destroy,
+};
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import connectDB from "./config/database.js";
 import userRoutes from "./routes/userRoutes.js"
 import typeUserRoutes from "./routes/typeUserRoutes.js"
+import reservationRoutes from "./routes/reservationRoutes.js"
 
 const app = express();
 const port = 3000;
@@ -13,6 +14,7 @@ connectDB();
 //rutas
 app.use(userRoutes);
 app.use(typeUserRoutes);
+app.use(reservationRoutes);
 
 
 app.listen(port, () => {
